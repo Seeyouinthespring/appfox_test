9d0c844502b2c79cbf48819429e3f3d650b0f6b6
colya <colya.juravlyov2011@ya.ru>
2022-08-11 03:06:43 +0300

    API key from TD used

--- lib/network/endpoints.dart
+++ lib/network/endpoints.dart
@@ -3,5 +3,5 @@ class Endpoints {
 
   static const String server = "https://quizapi.io/api/v1/";
   static const String questions = "questions";
-  static const String APIkey= "SnLFVUdx55dWA8F6KJmtEqwsKWERCZv1VEWZ1gDX";
+  static const String APIkey= "j24WhINsXuMG7PszLmbkLHqRiXRoFnjRZrHxkwDa";
 }
